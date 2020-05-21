e76248e4c411e0dbbb4b66034432692bd53bb704
jahorton <jahorton@sil.org>
2020-05-21 17:31:44 +0700

    fix: reverts unneeded changes in Version test-case

--- tests/versions.js
+++ tests/versions.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert;
 
 // Verifies that we can access Version directly from the base-level module import file.
-let Version = require('../dist').utils.Version;
+let Version = require('../dist').utils;
 
 // // Alternatively, requires the module_bundle, which auto-exports the namespace.
 // // We can't require the bundle, as it's been webpack'd and thus doesn't do actual module exports.
@@ -11,28 +11,28 @@ let Version = require('../dist').utils.Version;
 
 describe('Version Logic', function() {
   it('Should provide a default, fallback value when nothing is specified', function() {
-    var fallback = new Version(undefined);
-    assert.isTrue(fallback.equals(Version.FIRST_ALPHA));
+    var fallback = new utils.Version(undefined);
+    assert.isTrue(fallback.equals(utils.Version.FIRST_ALPHA));
   });
 
   it('Should properly process a simple major.minor version string.', function() {
-    var version = new Version("1.2");
+    var version = new utils.Version("1.2");
     assert.equal(version.major, 1);
     assert.equal(version.minor, 2);
   });
 
   it('Should handle long/deep version specifications.', function() {
-    var version = new Version("1.2.3.4.5.6");
+    var version = new utils.Version("1.2.3.4.5.6");
     assert.equal(version.components.length, 6);
     assert.equal(version.major, 1);
     assert.equal(version.minor, 2);
   });
 
   it('Should properly compare two versions.', function() {
-    var v9_0_1 = new Version("9.0.1");
-    var v9_1_0 = new Version("9.1.0");
-    var v10_0 = new Version("10.0");
-    var v10_0_0 = new Version("10.0.0");
+    var v9_0_1 = new utils.Version("9.0.1");
+    var v9_1_0 = new utils.Version("9.1.0");
+    var v10_0 = new utils.Version("10.0");
+    var v10_0_0 = new utils.Version("10.0.0");
 
     // "Precede" checks
     assert.equal(v9_0_1.compareTo(v9_1_0), -1);
